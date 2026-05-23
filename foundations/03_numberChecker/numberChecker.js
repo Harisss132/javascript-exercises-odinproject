89d5eb5ffcdedb1487f99f70aeca7cf2ba8792d5
Harisss132 <zaktwod123@gmail.com>
2026-05-23 17:31:18 +0700

    exercises 1-5

--- foundations/03_numberChecker/numberChecker.js
+++ foundations/03_numberChecker/numberChecker.js
@@ -1,5 +1,7 @@
+const addNumbers = require("../02_addNumbers/addNumbers");
+
 function numberChecker(number) {
-  if (number === 6) {
+  if (number >= 6) {
     return true;
   } else {
     return false;
